06e4396ca17dc16a569499079bd4366ebb1c13c6
Elizabeth2107 <elizabethndlovu2107@gmail.com>
2024-02-01 12:24:26 +0000

    Three files added
    This app was built to practice css grid and javascript

---
+++ index.js
@@ -0,0 +1,109 @@
+const userInput = document.getElementById("user-input")
+
+let count = 0
+
+function oneBtn(){
+    count = 1
+    userInput.value += count
+}
+
+function twoBtn(){
+    count = 2
+    userInput.value += count
+}
+
+function threeBtn(){
+    count = 3
+    userInput.value += count
+}
+
+function fourBtn(){
+    count = 4
+    userInput.value += count
+}
+
+function fiveBtn(){
+    count = 5
+    userInput.value += count
+}
+
+function sixBtn(){
+    count = 6
+    userInput.value += count
+}
+
+function sevenBtn(){
+    count = 7
+    userInput.value += count
+}
+
+function eightBtn(){
+    count = 8
+    userInput.value += count
+}
+
+function nineBtn(){
+    count = 9
+    userInput.value += count
+}
+
+function zeroBtn(){
+    count = 0
+    userInput.value += count
+}
+
+function resetBtn(){
+    count=''
+    userInput.value = count
+}
+
+function subBtn(){
+    count='-'
+    userInput.value += count
+}
+
+function divideBtn(){
+    count='/'
+    userInput.value += count
+}
+
+function multiBtn(){
+    count='*'
+    userInput.value += count
+}
+
+function commaBtn(){
+    count="."
+    userInput.value += count
+}
+
+function plusBtn(){
+    count = "+" 
+    userInput.value += count    
+}
+
+function equalBtn(){
+    userInput.value = eval(userInput.value)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+
+
